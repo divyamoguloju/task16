e3f5640351dd422bf74e8597699a3faf83229287
divyamoguloju <92846806+divyamoguloju@users.noreply.github.com>
2025-03-12 21:04:52 -0500

    Update script1.js

--- script1.js
+++ script1.js
@@ -1,7 +1,7 @@
 const works = {
     "task1": { "title": "Project 1", "url": "works/task1/index.html" },
-    "task2": { "title": "Project 2", "url": "works/task2/index.html" },
-    "task3": { "title": "Project 3", "url": "works/task3/index.html" },
+    "task2": { "title": "Project 2", "url": "works/task2/index1.html" },
+    "task3": { "title": "Project 3", "url": "works/task3/index2.html" },
 };
 
 function loadProject(task, bodyColor, titleColor) {
